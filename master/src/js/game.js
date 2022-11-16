8f27e465dce854c272075f4896df3ddd763ddeb1
LucasWiggie <81293638+LucasWiggie@users.noreply.github.com>
2022-11-16 14:42:10 +0100

    Update game.js

--- master/src/js/game.js
+++ master/src/js/game.js
@@ -3,7 +3,7 @@ var config = {
     width: 800,
     height: 600,
     backgroundColor: 0x000000,
-    scene: [Scene1],
+    scene: [Level1],
     physics: {
         default: 'arcade',
         arcade: {
