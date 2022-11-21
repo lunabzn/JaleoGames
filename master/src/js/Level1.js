28a5c2396d5d3a1dd342b196dcfbd7a34a301e4c
LucasWiggie <81293638+LucasWiggie@users.noreply.github.com>
2022-11-21 11:01:10 +0100

    Add files via upload

--- master/src/js/Level1.js
+++ master/src/js/Level1.js
@@ -5,7 +5,7 @@ class Level1 extends Phaser.Scene {
     }
 
     preload() {
-        this.load.spritesheet('guy', '../../resources/img/heroDef.png', { frameWidth: 50, frameHeight: 65 });
+        this.load.spritesheet('vivo', '../../resources/img/spritesVivo.png', { frameWidth: 310, frameHeight: 331 });
         this.load.image('sky', '../../resources/img//sky.png');
     }
 
@@ -13,15 +13,11 @@ class Level1 extends Phaser.Scene {
         this.background = this.add.image(400, 300, 'sky');
 
         // Creación de los dos personajes
-        this.player1 = this.physics.add.sprite(100, 200, 'guy');
-        this.player2 = this.physics.add.sprite(100, 400, 'guy');
-
-        // this.player1.setScale(2);
-        // this.player2.seScale(2);
-
-        // Prueba de enemigo
-        // this.enemy = this.physics.add.sprite(500, 300, 'guy');
-        // this.enemy.setScale(2);
+        this.player1 = this.physics.add.sprite(100, 200, 'vivo');
+        this.player2 = this.physics.add.sprite(100, 400, 'vivo');
+        
+        this.player1.turnedLeft = false;
+        this.player2.turnedLeft = false;
 
         // Controles de los dos jugadores
         // J1: ASDW
@@ -38,41 +34,68 @@ class Level1 extends Phaser.Scene {
         // Animaciones de movimiento de los jugadores
         this.anims.create({
             key: 'left',
-            frames: this.anims.generateFrameNumbers('guy', { start: 12, end: 17 }),
+            frames: this.anims.generateFrameNumbers('vivo', { start: 5, end: 8 }),
             frameRate: 10,
             repeat: -1
         });
 
         this.anims.create({
-            key: 'turn',
-            frames: [{ key: 'guy', frame: 4 }],
+            key: 'turnLeft',
+            frames: [{ key: 'vivo', frame: 9 }],
             frameRate: 20
         });
 
         this.anims.create({
             key: 'right',
-            frames: this.anims.generateFrameNumbers('guy', { start: 5, end: 8 }),
+            frames: this.anims.generateFrameNumbers('vivo', { start: 11, end: 14 }),
             frameRate: 10,
             repeat: -1
         });
 
         this.anims.create({
-            key: 'up',
-            frames: this.anims.generateFrameNumbers('guy', { start: 5, end: 8 }),
+            key: 'turnRight',
+            frames: [{ key: 'vivo', frame: 10 }],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'upLeft',
+            frames: this.anims.generateFrameNumbers('vivo', { start: 5, end: 8 }),
             frameRate: 10,
             repeat: -1
         });
 
         this.anims.create({
-            key: 'down',
-            frames: this.anims.generateFrameNumbers('guy', { start: 5, end: 8 }),
+            key: 'upRight',
+            frames: this.anims.generateFrameNumbers('vivo', { start: 11, end: 14 }),
             frameRate: 10,
             repeat: -1
         });
 
         this.anims.create({
-            key: 'attack',
-            frames: this.anims.generateFrameNumbers('guy', { start: 4, end: 4 }),
+            key: 'downLeft',
+            frames: this.anims.generateFrameNumbers('vivo', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'downRight',
+            frames: this.anims.generateFrameNumbers('vivo', { start: 11, end: 14 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'attackLeft',
+            frames: this.anims.generateFrameNumbers('vivo', { start: 3, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'attackRight',
+            frames: this.anims.generateFrameNumbers('vivo', { start: 4, end: 4 }),
             frameRate: 10,
             repeat: -1
         });
@@ -83,58 +106,76 @@ class Level1 extends Phaser.Scene {
         // Eventos de controles del JUGADOR 1
         if (this.keyA.isDown) {
             this.player1.setVelocityX(-160);
+            this.player1.turnedLeft = true;
             this.player1.play('left', true);
         }
         else if (this.keyD.isDown) {
             this.player1.setVelocityX(160);
+            this.player1.turnedLeft = false;
             this.player1.play('right', true);
         }
         else if (this.keyW.isDown) {
             this.player1.setVelocityY(-160);
-            this.player1.play('up', true);
+            if(this.player1.turnedLeft){
+                this.player1.play('upLeft', true);
+            } else {
+                this.player1.play('upRight', true);
+            }
         }
         else if (this.keyS.isDown) {
             this.player1.setVelocityY(160);
-            this.player1.play('down', true);
+            if(this.player1.turnedLeft){
+                this.player1.play('downLeft', true);
+            } else {
+                this.player1.play('downRight', true);
+            }
         }
         else {
             this.player1.setVelocityX(0);
             this.player1.setVelocityY(0);
-            this.player1.play('turn');
+            if(this.player1.turnedLeft){
+                this.player1.play('turnLeft');
+            } else {
+                this.player1.play('turnRight');
+            }
         }
 
         // Ataque JUGADOR 1
         if(this.atkP1.isDown){
-            this.player1.play('attack');
+            if(this.player1.turnedLeft){
+                this.player1.play('attackLeft');
+            } else {
+                this.player1.play('attackRight');
+            }
         }
 
-        // Eventos de controles del JUGADOR 2
-        if (this.cursors.left.isDown) {
-            this.player2.setVelocityX(-160);
-            this.player2.play('left', true);
-        }
-        else if (this.cursors.right.isDown) {
-            this.player2.setVelocityX(160);
-            this.player2.play('right', true);
-        }
-        else if (this.cursors.up.isDown) {
-            this.player2.setVelocityY(-160);
-            this.player2.play('up', true);
-        }
-        else if (this.cursors.down.isDown) {
-            this.player2.setVelocityY(160);
-            this.player2.play('down', true);
-        }
-        else {
-            this.player2.setVelocityX(0);
-            this.player2.setVelocityY(0);
-            this.player2.play('turn');
-        }
+        // // Eventos de controles del JUGADOR 2
+        // if (this.cursors.left.isDown) {
+        //     this.player2.setVelocityX(-160);
+        //     this.player2.play('left', true);
+        // }
+        // else if (this.cursors.right.isDown) {
+        //     this.player2.setVelocityX(160);
+        //     this.player2.play('right', true);
+        // }
+        // else if (this.cursors.up.isDown) {
+        //     this.player2.setVelocityY(-160);
+        //     this.player2.play('up', true);
+        // }
+        // else if (this.cursors.down.isDown) {
+        //     this.player2.setVelocityY(160);
+        //     this.player2.play('down', true);
+        // }
+        // else {
+        //     this.player2.setVelocityX(0);
+        //     this.player2.setVelocityY(0);
+        //     this.player2.play('turn');
+        // }
 
-        // Ataque JUGADOR 2
-        if(this.atkP2.isDown){
-            this.player2.play('attack');
-        }
+        // // Ataque JUGADOR 2
+        // if(this.atkP2.isDown){
+        //     this.player2.play('attack');
+        // }
     }
 
 }
