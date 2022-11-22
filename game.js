e300a0fbd127b9e1268be27f1e0b7884946ffdd7
Rafa <84155484+rafagcl01@users.noreply.github.com>
2022-11-23 00:40:18 +0100

    Add files via upload

---
+++ game.js
@@ -0,0 +1,9 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent:'Escenas',
+    scene : [mainScene,selectorModeScene,selectorLevelScene,selectorCharacterScene]
+};
+
+var game = new Phaser.Game(config);
---
+++ selectorLevelScene.js
@@ -0,0 +1,47 @@
+class selectorLevelScene extends Phaser.Scene {
+    constructor(){
+        super("selectorLevelScene");
+
+    }
+        preload() {
+            this.load.image('image-menu', 'assets/bg-image-menu.jpg');
+            this.load.image('volver', 'assets/botonvolver.png');
+            this.load.image('nivel1', 'assets/lvl1.png');
+            this.load.image('nivel2', 'assets/lvl2.png');
+            this.load.image('nivel3', 'assets/lvl3.png');
+        }
+
+    create(){
+        this.add.image(0,0,"image-menu").setOrigin(0).setScale(0.75);
+        let playButtonVolver = this.add.image(game.config.width/2,game.config.height/2+175,"volver").setScale(0.5);
+
+        let playButtonLvl1 = this.add.image(120,game.config.height/2,"nivel1").setScale(1);
+
+        let playButtonLvl2 = this.add.image(game.config.width/2,game.config.height/2-87,"nivel2").setScale(1);
+
+        let playButtonLvl3 = this.add.image(game.config.width-120,game.config.height/2,"nivel3").setScale(1);
+        var level =0;
+
+        playButtonVolver.setInteractive();
+        playButtonVolver.on('pointerdown',()=>{
+            
+            this.scene.start('selectorModeScene')
+        })
+
+        playButtonLvl1.setInteractive();
+        playButtonLvl1.on('pointerdown',()=>{
+            level=1;
+            this.scene.start('selectorCharacterScene',level)
+        })
+        playButtonLvl2.setInteractive();
+        playButtonLvl2.on('pointerdown',()=>{
+            level=2;
+            this.scene.start('selectorCharacterScene',level)
+        })
+        playButtonLvl3.setInteractive();
+        playButtonLvl3.on('pointerdown',()=>{
+            level=3;
+            this.scene.start('selectorCharacterScene',level)
+        })
+    }
+};
