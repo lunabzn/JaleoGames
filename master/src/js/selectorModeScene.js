de64a3e718a64be5b7118192992fece0518288b4
Rafa <84155484+rafagcl01@users.noreply.github.com>
2022-11-28 18:07:31 +0100

    Add files via upload

---
+++ master/src/js/selectorModeScene.js
@@ -0,0 +1,31 @@
+class selectorModeScene extends Phaser.Scene {
+    constructor(){
+        super("selectorModeScene");
+
+    }
+        preload(){
+            
+            this.load.image('volver', 'assets/botonvolver.png');
+            this.load.image('online', 'assets/online.png');
+            this.load.image('offline', 'assets/offline.png');
+            this.load.image('bg-image-menu', 'assets/bg-image-menu.jpg');
+        }
+    
+    create(){
+        this.add.image(0,0,"bg-image-menu").setOrigin(0).setScale(0.75);
+        let playButtonOffline = this.add.image(game.config.width/2+40,game.config.height/2-150,"offline").setScale(0.6);
+        let playButtonOnline = this.add.image(game.config.width/2+40,game.config.height/2+18,"online").setScale(0.6);
+        let playButtonVolver = this.add.image(game.config.width/2+40,game.config.height/2+150,"volver").setScale(0.6);
+       
+
+        playButtonVolver.setInteractive();
+        playButtonVolver.on('pointerdown',()=>{
+            this.scene.start('mainScene')
+        })
+        playButtonOffline.setInteractive();
+        playButtonOffline.on('pointerdown',()=>{
+           this.scene.start('selectorLevelScene')
+        })
+        
+    }
+};
