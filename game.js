30c2f6ba750ed1dfc38889a74c375e9b583420ab
Fabio <81293357+faviolado@users.noreply.github.com>
2022-11-28 15:13:37 +0100

    Add files via upload

---
+++ game.js
@@ -0,0 +1,16 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: 0x000000,
+    scene: [Level1],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
+};
+
+var game = new Phaser.Game(config);
