0baac9a0c2b3f159649c52ad8a112402feff698d
Rafa <84155484+rafagcl01@users.noreply.github.com>
2022-11-28 18:06:38 +0100

    Delete game.js

--- game.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    parent:'Escenas',
-    scene : [mainScene,selectorModeScene,selectorLevelScene,selectorCharacterScene]
-};
-
-var game = new Phaser.Game(config);
