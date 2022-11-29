2b64ff38c58877749a36bded1ed26e6ebc118994
LucasWiggie <81293638+LucasWiggie@users.noreply.github.com>
2022-11-29 22:15:07 +0100

    Delete master directory

--- master/src/js/game.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    parent:'Escenas',
-    scene : [mainScene,selectorModeScene,selectorLevelScene,Level1]
-};
-
-var game = new Phaser.Game(config);
