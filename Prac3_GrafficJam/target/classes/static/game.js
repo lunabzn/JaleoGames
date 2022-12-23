ad0353082603e94ddb9368301dceb5b30132a412
YerayDS <82967594+YerayDS@users.noreply.github.com>
2022-12-23 21:47:50 +0100

    Add files via upload

---
+++ Prac3_GrafficJam/target/classes/static/game.js
@@ -0,0 +1,21 @@
+var config = {
+    type: Phaser.AUTO,
+    parent: "videogame",
+    width: 800,
+    height: 600,
+    backgroundColor: 0x000000,
+    dom: {
+        createContainer: true
+    },
+    scene: [audioManager, mainScene, selectorModeScene, selectorLevelScene, Level1, pauseScene, gameOver, gameWin, Lobby],
+    physics: {
+        default: 'arcade', 
+        arcade: {
+            gravity: { y: 0 },
+            debug: false
+        }
+    },
+};
+
+var game = new Phaser.Game(config);
+
