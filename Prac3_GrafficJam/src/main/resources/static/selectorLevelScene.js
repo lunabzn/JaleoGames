919082efc29f11212b051fe45ab8f24f4931892a
YerayDS <82967594+YerayDS@users.noreply.github.com>
2022-12-23 21:41:01 +0100

    Add files via upload

---
+++ Prac3_GrafficJam/src/main/resources/static/selectorLevelScene.js
@@ -0,0 +1,52 @@
+class selectorLevelScene extends Phaser.Scene {
+    constructor() {
+        super("selectorLevelScene");
+
+    }
+    preload() {
+        this.load.image('image-menu', 'resources/bg-image-menu.jpg');
+        this.load.image('volver', 'resources/botonvolver.png');
+        this.load.image('nivel1', 'resources/nivel1.png');
+        this.load.image('nivel2', 'resources/nivel2.png');
+        this.load.image('nivel3', 'resources/nivel3.png');
+        this.load.audio("click", ["resources/click.mp3"]);
+    }
+
+    create() {
+        this.add.image(0, 0, "image-menu").setOrigin(0).setScale(0.75);
+        let playButtonVolver = this.add.image(game.config.width / 2, game.config.height / 2 + 175, "volver").setScale(0.5);
+
+        let playButtonLvl1 = this.add.image(120, game.config.height / 2, "nivel1").setScale(1);
+
+        let playButtonLvl2 = this.add.image(game.config.width / 2, game.config.height / 2 - 87, "nivel2").setScale(1);
+
+        let playButtonLvl3 = this.add.image(game.config.width - 120, game.config.height / 2, "nivel3").setScale(1);
+
+        this.clickSound = this.sound.add("click");
+
+        playButtonVolver.setInteractive();
+        playButtonVolver.on('pointerdown', () => {
+
+            this.clickSound.play();
+            this.scene.start('selectorModeScene')
+        })
+
+        playButtonLvl1.setInteractive();
+        playButtonLvl1.on('pointerdown', () => {
+            this.clickSound.play();
+            //this.scene.start('selectorCharacterScene',level)
+            this.scene.stop('Level1');
+            this.scene.start('Level1');
+        })
+        //playButtonLvl2.setInteractive();
+        //playButtonLvl2.on('pointerdown',()=>{
+        //    this.clickSound.play();
+        //    this.scene.start('selectorCharacterScene',level)
+        //})
+        //playButtonLvl3.setInteractive();
+        //playButtonLvl3.on('pointerdown',()=>{
+        //    this.clickSound.play();
+        //    this.scene.start('selectorCharacterScene',level)
+        //})
+    }
+};
---
+++ Prac3_GrafficJam/src/main/resources/static/selectorModeScene.js
@@ -0,0 +1,42 @@
+class selectorModeScene extends Phaser.Scene {
+    constructor() {
+        super("selectorModeScene");
+
+    }
+    preload() {
+
+        this.load.image('volver', 'resources/botonvolver.png');
+        this.load.image('online', 'resources/online.png');
+        this.load.image('offline', 'resources/offline.png');
+        this.load.image('bg-image-menu', 'resources/bg-image-menu.jpg');
+        this.load.audio("click", ["resources/click.mp3"]);
+    }
+
+    create() {
+        this.add.image(0, 0, "bg-image-menu").setOrigin(0).setScale(0.75);
+        let playButtonOffline = this.add.image(game.config.width / 2 + 40, game.config.height / 2 - 150, "offline").setScale(0.6);
+        let playButtonOnline = this.add.image(game.config.width / 2 + 40, game.config.height / 2, "online").setScale(0.6);
+        let playButtonVolver = this.add.image(game.config.width / 2 + 40, game.config.height / 2 + 150, "volver").setScale(0.6);
+
+        this.clickSound = this.sound.add("click");
+
+        playButtonVolver.setInteractive();
+        playButtonVolver.on('pointerdown', () => {
+            this.clickSound.play();
+            this.scene.start('mainScene')
+        })
+
+        playButtonOffline.setInteractive();
+        playButtonOffline.on('pointerdown', () => {
+            this.clickSound.play();
+            this.scene.start('selectorLevelScene')
+        })
+
+        playButtonOnline.setInteractive();
+        playButtonOnline.on('pointerdown', () => {
+            this.clickSound.play();
+            this.scene.start('Lobby')
+        })
+
+    }
+};
